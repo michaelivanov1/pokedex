c0aef33f00ec0f60e7949d8858edbf8c33567a02
DMahyuddin <mahyuddin.22dylan@gmail.com>
2022-12-29 13:15:16 -0500

    button start

--- script.js
+++ script.js
@@ -1,54 +1,68 @@
 const fetchPokemon = async () => {
+  let pokeArray = [];
 
-    let pokeArray = []
+  try {
+    for (let i = 1; i <= 50; i++) {
+      const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
 
-    try {
-        for (let i = 1; i <= 50; i++) {
-            const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-
-            await fetch(url).then((response) => { pokeArray.push(response.json()) })
-        }
-
-        Promise.all(pokeArray).then(results => {
-            const parsePokemon = results.map(data => ({
-                name: data.name.charAt(0).toUpperCase() + data.name.slice(1),
-                id: data.id,
-                image: data.sprites.versions['generation-v']['black-white'].animated['front_default'] || data.sprites['front_default'],
-                type: data.types.map((type) => type.type.name).join(', ')
-            }));
-            renderPokemon(parsePokemon)
-        })
-    } catch (err) {
-        console.error(`error fetching from api: ${err}`)
+      await fetch(url).then((response) => {
+        pokeArray.push(response.json());
+      });
     }
-}
+
+    Promise.all(pokeArray).then((results) => {
+      const parsePokemon = results.map((data) => ({
+        name: data.name.charAt(0).toUpperCase() + data.name.slice(1),
+        id: data.id,
+        image:
+          data.sprites.versions["generation-v"]["black-white"].animated[
+            "front_default"
+          ] || data.sprites["front_default"],
+        type: data.types.map((type) => type.type.name).join(", "),
+      }));
+      renderPokemon(parsePokemon);
+    });
+  } catch (err) {
+    console.error(`error fetching from api: ${err}`);
+  }
+};
+
+const onPokemonClick = (e) => {
+  console.log(e);
+};
 
 const renderPokemon = (pokemon) => {
+  // pokemon.map(p => {
+  //     console.log(p.type)
+  // })
 
-    // pokemon.map(p => {
-    //     console.log(p.type)
-    // })
-
-    const HTMLString = pokemon.map(p =>
+  const HTMLString = pokemon
+    .map(
+      (p) =>
         `
+        <button class="card-button" onclick=onPokemonClick(this)>
         <li class="card-full">
             <img class="card-image" src="${p.image}"/>
             <h3 class="card-id">${p.id}</h3>
             <h2 class="card-title">${p.name}</h2>
-            <p class="card-type">${p.type
-        // == 'fire'
-        // ?
-        // document.querySelector('.card-title').style.color = 'red'
-        // :
-        // document.querySelector('.card-title').style.color = "black"
-        }
+            <p class="card-type">${
+              p.type
+              // == 'fire'
+              // ?
+              // document.querySelector('.card-title').style.color = 'red'
+              // :
+              // document.querySelector('.card-title').style.color = "black"
+            }
             </p>
         </li>
+        </button>
+        
         `
-    ).join('');
+    )
+    .join("");
 
-    let ol = document.getElementById('pokedex');
-    ol.innerHTML = HTMLString;
-}
+  let ol = document.getElementById("pokedex");
+  ol.innerHTML = HTMLString;
+};
 
-fetchPokemon()
+fetchPokemon();
