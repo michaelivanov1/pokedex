f490b33b5a286bec12241bba1bc51b0593882fde
Michael Ivanov <m_ivanov82275@fanshaweonline.ca>
2022-12-30 21:04:38 -0500

    added a lot more styling

--- script.js
+++ script.js
@@ -1,126 +1,97 @@
+// fetch pokemon on page load
 const fetchPokemon = async () => {
-  let pokeArray = [];
+    let pokeArray = [];
+    try {
+        renderLoading();
+        // max count: 905
+        for (let i = 1; i <= 42; i++) {
+            const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
 
-  try {
-    renderLoading();
-    for (let i = 1; i <= 20; i++) {
-      const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-
-      await fetch(url).then((response) => {
-        pokeArray.push(response.json());
-      });
+            await fetch(url).then((response) => {
+                pokeArray.push(response.json());
+            });
+        }
+        Promise.all(pokeArray).then((results) => {
+            const parsePokemon = results.map((data) => ({
+                name: data.name.charAt(0).toUpperCase() + data.name.slice(1),
+                id: "#" + data.id,
+                image: data.sprites.versions["generation-v"]["black-white"].animated["front_default"] || data.sprites["front_default"],
+                type: data.types.map((type) => type.type.name).join(", "),
+                order: data.id,
+            }));
+            renderPokemon(parsePokemon);
+        });
+    } catch (err) {
+        console.error(`error fetching from api: ${err}`);
     }
-
-    Promise.all(pokeArray).then((results) => {
-      const parsePokemon = results.map((data) => ({
-        name: data.name.charAt(0).toUpperCase() + data.name.slice(1),
-        id: "#" + data.id,
-        image:
-          data.sprites.versions["generation-v"]["black-white"].animated[
-            "front_default"
-          ] || data.sprites["front_default"],
-        type: data.types.map((type) => type.type.name).join(", "),
-        order: data.id,
-      }));
-      renderPokemon(parsePokemon);
-    });
-  } catch (err) {
-    console.error(`error fetching from api: ${err}`);
-  }
 };
 
+// fired upon clicking a card
 const onPokemonClick = async (e) => {
-  let currJson;
-  try {
-    const url = `https://pokeapi.co/api/v2/pokemon/${e.id}`;
-    await fetch(url).then((response) => {
-      currJson = response.json();
-    });
-    Promise.resolve(currJson).then((results) => {
-      const currPokemon = {
-        name: results.name.charAt(0).toUpperCase() + results.name.slice(1),
-        id: "#" + results.id,
-        image:
-          results.sprites.versions["generation-v"]["black-white"].animated[
-            "front_default"
-          ] || data.sprites["front_default"],
-        type: results.types.map((type) => type.type.name).join(", "),
-        order: results.order,
-      };
-      currentPokemonInfo(currPokemon);
-    });
-  } catch (err) {
-    console.error(`error fetching from api: ${err}`);
-  }
+    let currJson;
+    try {
+        const url = `https://pokeapi.co/api/v2/pokemon/${e.id}`;
+        await fetch(url).then((response) => {
+            currJson = response.json();
+        });
+        Promise.resolve(currJson).then((results) => {
+            const currPokemon = {
+                name: results.name.charAt(0).toUpperCase() + results.name.slice(1),
+                id: "#" + results.id,
+                image: results.sprites.other['dream_world']['front_default'] || results.sprites["front_default"],
+                type: results.types.map((type) => type.type.name).join(", "),
+                order: results.order,
+            };
+            currentPokemonInfo(currPokemon);
+        });
+    } catch (err) {
+        console.error(`error fetching from api: ${err}`);
+    }
 };
 
+// run animation while pokemon are loading
 const renderLoading = () => {
-  const HTMLString = `
-  <h1>loading...</h1>
-  `;
-
-  let ol = document.getElementById("pokedex");
-  ol.innerHTML = HTMLString;
-};
-
-const renderPokemon = (pokemon) => {
-  // pokemon.map(p => {
-  //     console.log(p.type)
-  // })
-
-  const HTMLString = pokemon
-    .map(
-      (p) =>
+    const HTMLString =
         `
-        <button class="card-full" onclick=onPokemonClick(this) id="${p.order}">
-        <li class="card-li">
-            <img class="card-image" src="${p.image}"/>
-            <h3 class="card-id">${p.id}</h3>
-            <h2  class="card-title" >${p.name}</h2>
-            <p class="card-type">${
-              p.type
-              // == 'fire'
-              // ?
-              // document.querySelector('.card-title').style.color = 'red'
-              // :
-              // document.querySelector('.card-title').style.color = "black"
-            }
-            </p>
-        </li>
-        </button>
-        `
-    )
-    .join("");
-
-  let ol = document.getElementById("pokedex");
-  ol.innerHTML = HTMLString;
-};
-
-const currentPokemonInfo = (pokemon) => {
-  console.log(pokemon);
-  const HTMLString = `
-        <button class="card-full" onclick=onPokemonClick(this) id="${
-          pokemon.order
-        }">
-        <li class="card-li">
-            <img class="card-image" src="${pokemon.image}"/>
-            <h3 class="card-id">${pokemon.id}</h3>
-            <h2  class="card-title" >${pokemon.name}</h2>
-            <p class="card-type">${
-              pokemon.type
-              // == 'fire'
-              // ?
-              // document.querySelector('.card-title').style.color = 'red'
-              // :
-              // document.querySelector('.card-title').style.color = "black"
-            }
-            </p>
-        </li>
-        </button>
+        <h1>loading...</h1>
         `;
 
-  let info = document.getElementById("pokemon-info");
-  info.innerHTML = HTMLString;
+    let ol = document.getElementById("pokedex");
+    ol.innerHTML = HTMLString;
+};
+
+// render pokemon cards
+const renderPokemon = (pokemon) => {
+
+    const HTMLString = pokemon.map((p) => `
+        <button class="card-full" onclick=onPokemonClick(this) id="${p.order}">
+            <li class="card-li">
+                <img class="card-image" src="${p.image}"/>
+                <h3 class="card-id">${p.id}</h3>
+                <h2  class="card-title" >${p.name}</h2>
+                <p class="card-type">${p.type}</p>
+            </li>
+        </button>
+        `).join("");
+
+    let ol = document.getElementById("pokedex");
+    ol.innerHTML = HTMLString;
+};
+
+// the display that comes up for currently selected pokemon
+const currentPokemonInfo = (pokemon) => {
+    console.log(pokemon);
+    const HTMLString = `
+        <div class="selected-card-full"> 
+            <img class="selected-card-image" src="${pokemon.image}"/>
+            <h3 class="selected-card-id">${pokemon.id}</h3>
+            <h2 class="selected-card-title" >${pokemon.name}</h2>
+            <p class="selected-card-type">${pokemon.type}</p>
+        </div>
+        `;
+
+    let info = document.getElementById("pokemon-info");
+    info.innerHTML = HTMLString;
 };
 
 fetchPokemon();
